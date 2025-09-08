5aab7211d4d29cbe010d4442503038f1c4dd7dd5
Gabriel Gomes <gabriel.gomes.brasil@gmail.com>
2025-09-08 19:56:46 -0300

    Adicionado exercicio de condições

---
+++ exercicio-01-condicoes/velocidade.js
@@ -0,0 +1,5 @@
+var vel = 60.5
+console.log(`a velocidade do seu carro é ${vel} km/h`)
+if (vel > 40) { // condição simples
+    console.log(`Você está muito rápido! sua velocidade é de ${vel}km/h multado!`)
+}
