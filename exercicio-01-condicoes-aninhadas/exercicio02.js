fdba7556f4da4b408c77d51336d9046c1b94a5c1
Gabriel Gomes <gabriel.gomes.brasil@gmail.com>
2025-09-09 20:13:38 -0300

    Adicionado condições aninhadas

---
+++ exercicio-01-condicoes-aninhadas/exercicio02.js
@@ -0,0 +1,10 @@
+var agora = new Date()
+var hora = 22
+console.log (`agora são exatamente ${hora} hora`)
+if (hora < 12) {
+    console.log ('bom dia!')
+} else if (hora <= 18) {
+    console.log ('boa tarde!') 
+} else {
+    console.log('boa noite!')
+}
