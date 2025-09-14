8d7e432e5c64b03e086e8a8e28df96e808fe955a
Gabriel Gomes <gabriel.gomes.brasil@gmail.com>
2025-09-14 08:25:34 -0300

    Estrutura de repetição While no inicio de no fim

---
+++ exercicios-javascript/exercico-01-repeticoes/repeticao-while-no-inicio-e-no-final.js
@@ -0,0 +1,18 @@
+
+// Estrutura de repetição com teste logico no inicio
+var c = 1
+while (c <= 6) {
+ console.log(`Essa é  a ${c}ª vez que o contador foi avaliado no inicio`)
+ c++ // c = c + 1 [ operadores ]
+}
+
+
+// Estrutura de repetição com teste logico no final
+var c = 1
+do {
+ console.log(`Essa é  a ${c}ª vez que o contador foi avaliado no final`)
+ c++ // c = c + 1 [ operadores ]
+} while (c <= 6)
+
+ 
+
