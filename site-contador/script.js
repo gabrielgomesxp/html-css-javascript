d69d0e4156badde8cc88e74c16647a25c7f45b60
Gabriel Gomes <gabriel.gomes.brasil@gmail.com>
2025-09-15 06:02:05 -0300

    atualização no js site-contador

--- site-contador/script.js
+++ site-contador/script.js
@@ -8,22 +8,32 @@
             // adificonando evento ao botao
             btncalc.addEventListener('click', calcularevento)
 
-            function calcularevento() {  // buscando o valor numero
+            function calcularevento() {
+                // mensagem de erro caso não haja valor nas div
+                if (start.value.length == 0 || end.value.length == 0 || pass.value.length == 0) {
+                    window.alert('Preencha todos os campos antes de calcular!')
+                }
+                // buscando apenas o valor numererico
                 const n1 = Number(start.value);
                 const n2 = Number(end.value);
-                const n3 = Number(pass.value);
+                let n3 = Number(pass.value);
+
+                if (n3 <= 0) {
+                alert('Passo inválido! Considerando PASSO = 1');
+                n3 = 1;
+                }
 
                 // removendo frase ao executar ação do botao
                 contagem.innerHTML = '';
 
-                if ( n1 < n2) { // contagem do maior para o menor
-                     for(var i = n1; i <=n2; i += n3) {
+                if ( n1 < n2) { // contagem do menor para o maior
+                     for(let i = n1; i <=n2; i += n3) {
                     const listItem = document.createElement('li');
                     listItem.textContent = i;
                     contagem.appendChild(listItem);
                     }
-                } else { // contagem do menor para o maior
-                    for(var i = n1; i >= n2; i -= n3) {
+                } else { // contagem do maior para o menor
+                    for(let i = n1; i >= n2; i -= n3) {
                     const listItem = document.createElement('li');
                     listItem.textContent = i;
                     contagem.appendChild(listItem);   
